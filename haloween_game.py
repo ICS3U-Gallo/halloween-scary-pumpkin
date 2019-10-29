0fc4c18eaff868bf44b29b7bb4684ab37b01815e
nlaids <47423885+nlaids@users.noreply.github.com>
2019-10-29 17:12:15 -0400

    Update haloween_game.py
    - Base game done
    - Everything is functional 
    - Still need different screens (instruction, title screen, game over)
    - Still need additional game elements (increasing speed)
    - Check out link for preview

--- haloween_game.py
+++ haloween_game.py
@@ -4,17 +4,17 @@ import random
 WIDTH = 640
 HEIGHT = 480
 
-up_pressed = False
-down_pressed = False
 left_pressed = False
 right_pressed = False
 
 window = arcade.open_window(WIDTH, HEIGHT, "Halloween Candy Drop Game")
+
 background = arcade.Sprite('sprites/background.png', center_x=320, center_y=240, scale=1)
 
 candy = arcade.Sprite('sprites/candy.png', center_x=random.randint(1, 640), center_y=HEIGHT, scale=0.4)
-candy.change_y = -5
-basket = arcade.Sprite('sprites/basket.png', center_x=WIDTH / 15, center_y=128, scale=0.7)
+candy.change_y = -7
+
+basket = arcade.Sprite('sprites/basket.png', center_x=WIDTH / 2, center_y=128, scale=0.7)
 
 score = 0
 start_y = 220
@@ -28,20 +28,25 @@ def setup():
 
 
 def update(delta_time, ):
-    global score
-    candy.update()
-    kill = candy.kill()
+    global score, hit
     hit = arcade.check_for_collision(basket, candy)
-
+    candy.update()
+    if candy.center_y == 130:
+        candy.center_y = HEIGHT
+        candy.center_x = random.randint(1, 640)
     if left_pressed:
-        basket.center_x -= 6
+        basket.center_x -= 12
     if right_pressed:
-        basket.center_x += 6
-    if basket.center_x > 550:
+        basket.center_x += 12
+    if basket.center_x > 600:
         basket.center_x -= 20
     if basket.center_x < 50:
         basket.center_x += 20
-
+    if hit:
+        candy.center_y = HEIGHT
+        candy.center_x = random.randint(1, 640)
+        score += 1
+        
 
 
 @window.event
@@ -50,25 +55,27 @@ def on_draw():
     candy.draw()
     basket.draw()
     arcade.draw_text(f"Score : {score}", 250, 35, arcade.color.BLACK, 25, )
-    if candy.center_y == 130:
-        candy.center_y += HEIGHT
-        candy.center_x = random.randint(1, 640)
 
 
 @window.event
 def on_key_press(key, modifiers):
-    pass
-
-
-@window.event
-def on_key_release(key, modifiers):
-    global up_pressed, down_pressed, left_pressed, right_pressed
-
+    global left_pressed, right_pressed
     if key == arcade.key.D:
         right_pressed = True
 
     if key == arcade.key.A:
         left_pressed = True
+
+
+@window.event
+def on_key_release(key, modifiers):
+    global left_pressed, right_pressed
+
+    if key == arcade.key.D:
+        right_pressed = False
+
+    if key == arcade.key.A:
+        left_pressed = False
 
 
 @window.event
@@ -78,6 +85,3 @@ def on_mouse_press(x, y, button, modifiers):
 
 if __name__ == '__main__':
     setup()
-
-
-
